78da517c82b77d72e2a77f05f3a368a774743a1f
Sepand KASHANI <sepand@kashani.ch>
2025-05-14 19:47:12 +0200

    [dvc] Cache paths across runs.
    This allows one to avoid the cost of recomputing contractions per call.

--- dvc.py
+++ dvc.py
@@ -1,6 +1,11 @@
 import numpy as np
 import opt_einsum as oe
 
+MA_Contract = tuple[
+    "oe.contract.ContractExpression",
+    "oe.contract.ContractExpression",
+]
+
 
 def computeDICoperators(
     im1: np.ndarray,  # f32(Nz, Ny, Nx)
@@ -14,7 +19,7 @@ def computeDICoperators(
     # Different implementation of `spambind.DIC.DICToolkit.computeDICoperators()`.
 
     im2g = np.stack([im2gz, im2gy, im2gx], axis=0)  # (3, Nz, Ny, Nx)
-    _M, _A = _computeDICoperators(im1, im2, im2g)
+    _M, _A, _ = _computeDICoperators(im1, im2, im2g)
     M[:] = _M.reshape(12, 12)
     A[:] = _A.reshape(12)
 
@@ -23,7 +28,8 @@ def _computeDICoperators(
     im1: np.ndarray,  # f32(Nz, Ny, Nx)
     im2: np.ndarray,  # f32(Nz, Ny, Nx)
     im2g: np.ndarray,  # f32(3, Nz, Ny, Nx)
-) -> tuple[np.ndarray, np.ndarray]:
+    expr: MA_Contract = None,  # (M_expr, A_expr)
+) -> tuple[np.ndarray, np.ndarray, MA_Contract]:
     # Low-level backend of computeDICoperators().
     #
     # Returns
@@ -32,13 +38,16 @@ def _computeDICoperators(
     #     (3, 4, 3, 4)
     # A: ndarray[f64]
     #     (3, 4)
+    # (M_expr, A_expr): MA_Contract
+    #     Good contraction rules to eval (M, A).
+    #     Re-use them between calls to avoid the cost of finding good contractions each time.
 
     assert im1.ndim == 3
     assert im1.shape == im2.shape
     assert im2g.shape == (3, *im1.shape)
     assert all(_.dtype is np.dtype(np.single) for _ in (im1, im2, im2g))
 
-    sh = im1.shape  # (Nz, Ny, Nx)
+    sh = (Nz, Ny, Nx) = im1.shape  # (Nz, Ny, Nx)
     x = [None] * 3
     for i in range(3):
         N = sh[i]
@@ -46,28 +55,41 @@ def _computeDICoperators(
         _x[i] = np.arange(N) - 0.5 * (N - 1)
         x[i] = _x
 
-    oe_kwargs = dict(
-        use_blas=True,
-        optimize="auto",
-    )
+    if expr:
+        M_expr, A_expr = expr
+    else:
+        oe_kwargs = dict(
+            use_blas=True,
+            optimize="greedy",  # seems to work best during my tests
+        )
 
-    nan_mask = np.isnan(im1) | np.isnan(im2)
-    A = oe.contract(
-        "qrs,qrs,iqrs,jq,jr,js->ij",
-        ~nan_mask,
-        np.nan_to_num(im1 - im2),
-        im2g,
-        *x,
-        **oe_kwargs,
-    )
-    M = oe.contract(
-        "qrs,iqrs,jq,jr,js,kqrs,lq,lr,ls->ijkl",
-        ~nan_mask,
-        im2g,
-        *x,
-        im2g,
-        *x,
-        **oe_kwargs,
-    )
+        M_expr = oe.contract_expression(
+            "qrs,iqrs,jq,jr,js,kqrs,lq,lr,ls->ijkl",
+            sh,
+            (3, *sh),
+            (4, Nz),
+            (4, Ny),
+            (4, Nx),
+            (3, *sh),
+            (4, Nz),
+            (4, Ny),
+            (4, Nx),
+            **oe_kwargs,
+        )
+        A_expr = oe.contract_expression(
+            "qrs,qrs,iqrs,jq,jr,js->ij",
+            sh,
+            sh,
+            (3, *sh),
+            (4, Nz),
+            (4, Ny),
+            (4, Nx),
+            **oe_kwargs,
+        )
+        expr = (M_expr, A_expr)
 
-    return M, A
+    nan_mask = ~(np.isnan(im1) | np.isnan(im2))
+    M = M_expr(nan_mask, im2g, *x, im2g, *x)
+    A = A_expr(nan_mask, np.nan_to_num(im1 - im2), im2g, *x)
+
+    return M, A, (M_expr, A_expr)
